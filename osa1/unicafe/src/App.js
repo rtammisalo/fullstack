f46ee7b4b13a7b4616c74d00e33277c00271abdd
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2022-07-24 22:33:41 +0300

    Lisää 1.10: unicafe, step 5

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 
+const StatisticsLine = ({ text, value }) => (
+  <>
+    {text} {value} <br />
+  </>
+)
+
 const Statistics = (props) => {
-  const countReviews = () => props.good + props.neutral + props.bad
+  const allReviews = props.good + props.neutral + props.bad
   const goodValue = 1
   const badValue = -1
+  const averageReview = (props.good * goodValue + props.bad * badValue)
+    / allReviews
+  const positivePercent = props.good / allReviews * 100
 
-  if (countReviews() === 0) {
+  if (allReviews === 0) {
     return (
       <>
         <h1>statistics</h1>
@@ -18,29 +27,34 @@ const Statistics = (props) => {
     <>
       <h1>statistics</h1>
       <p>
-        good {props.good}<br />
-        neutral {props.neutral}<br />
-        bad {props.bad}<br />
-        all {countReviews()}<br />
-        average {(props.good * goodValue + props.bad * badValue) / countReviews()} <br />
-        positive {props.good / countReviews() * 100} %<br />
+        <StatisticsLine text='good' value={props.good} />
+        <StatisticsLine text='neutral' value={props.neutral} />
+        <StatisticsLine text='bad' value={props.bad} />
+        <StatisticsLine text='all' value={allReviews} />
+        <StatisticsLine text='average' value={averageReview} />
+        <StatisticsLine text='positive' value={positivePercent + ' %'} />
       </p>
     </>
   )
 }
+
+const Button = ({ handleClick, buttonText }) => (
+  <button onClick={handleClick}> {buttonText} </button>
+)
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const handleClick = (value, setValue) => () => setValue(value + 1)
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button handleClick={handleClick(good, setGood)} buttonText='good' />
+      <Button handleClick={handleClick(neutral, setNeutral)} buttonText='neutral' />
+      <Button handleClick={handleClick(bad, setBad)} buttonText='bad' />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
