bff7168428c717ba866d7be2084d388373cc5ff1
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2022-08-02 17:04:18 +0300

    Lisää 4.5 apufunktioita ja yksikkötestejä

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -7,7 +7,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((favorite, blog) => {
+    if (!favorite || blog.likes > favorite.likes) {
+      return blog
+    }
+    return favorite
+  }, null)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
