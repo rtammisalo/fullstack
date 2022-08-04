34e619270bf02453a2f9f06fc8cb0be582c9632b
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2022-08-04 22:15:06 +0300

    Lisää 4.18: blogilistan laajennus (token login)

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 
@@ -13,6 +14,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
---
+++ osa4/blogilista/controllers/login.js
@@ -0,0 +1,23 @@
+const loginRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const UserModel = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body
+  const user = await UserModel.findOne({ username })
+
+  if (!user || !await bcrypt.compare(password, user.passwordHash)) {
+    return response.status(401).json('invalid username or password')
+  }
+
+  const tokenData = {
+    username: user.username,
+    id: user._id
+  }
+  const token = jwt.sign(tokenData, process.env.SECRET)
+
+  response.status(200).send({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
