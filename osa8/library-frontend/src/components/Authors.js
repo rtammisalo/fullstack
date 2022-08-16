f5902c9bace6bb0fcb199c40f383b8dd7465d7a8
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2022-08-16 17:11:52 +0300

    Lisää 8.8 kirjailijoiden näkymä

---
+++ osa8/library-frontend/src/components/Authors.js
@@ -0,0 +1,36 @@
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../queries'
+
+const Authors = (props) => {
+  const allAuthorsData = useQuery(ALL_AUTHORS, { skip: !props.show })
+
+  if (!props.show || allAuthorsData.loading) {
+    return null
+  }
+
+  const authors = allAuthorsData.data.allAuthors
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Authors
