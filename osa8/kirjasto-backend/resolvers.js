686d4c329f74e654b18bedf26de394f5be112e33
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2022-08-18 12:43:23 +0300

    Refaktoroi gql&schemat

---
+++ osa8/kirjasto-backend/resolvers.js
@@ -0,0 +1,125 @@
+const { UserInputError, AuthenticationError } = require('apollo-server')
+const Author = require('./models/author')
+const Book = require('./models/book')
+const User = require('./models/user')
+const { JWT_SECRET } = require('./utils/config')
+const jwt = require('jsonwebtoken')
+
+const resolvers = {
+  Query: {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      const filters = {}
+
+      if (args.author) {
+        const author = await Author.findOne({ name: args.author })
+
+        if (!author) {
+          return []
+        }
+
+        filters.author = author._id
+      }
+
+      if (args.genre) {
+        filters.genres = { $in: [args.genre] }
+      }
+
+      return Book.find(filters)
+    },
+    allAuthors: async () => Author.find({}),
+    me: async (root, args, context) => {
+      return context.currentUser
+    },
+  },
+  Author: {
+    bookCount: async ({ id }) => {
+      const books = await Book.find({ author: id })
+      return books.length
+    },
+  },
+  Book: {
+    author: async ({ author }) => {
+      return Author.findById(author)
+    },
+  },
+  Mutation: {
+    createUser: async (root, args) => {
+      if (await User.findOne({ username: args.username })) {
+        throw new UserInputError(`Username ${args.username} is in use`)
+      }
+
+      const user = new User({ ...args })
+
+      try {
+        await user.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+
+      return user
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'salami') {
+        throw new UserInputError('incorrect username/password')
+      }
+
+      return {
+        value: jwt.sign({ username: user.username, id: user._id }, JWT_SECRET),
+      }
+    },
+    addBook: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new AuthenticationError('No valid authentication token')
+      }
+
+      const book = new Book({ ...args })
+      let author = await Author.findOne({ name: args.author })
+
+      try {
+        if (!author) {
+          author = new Author({ name: args.author })
+          await author.save()
+        }
+
+        book.author = author._id
+        await book.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+
+      return book
+    },
+    editAuthor: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new AuthenticationError('No valid authentication token')
+      }
+
+      const author = await Author.findOne({ name: args.name })
+
+      if (!author) {
+        return null
+      }
+
+      try {
+        author.born = args.setBornTo
+        await author.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+
+      return author
+    },
+  },
+}
+
+module.exports = resolvers
