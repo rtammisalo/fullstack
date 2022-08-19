83f9f4a963d02c7812276a8f97fbd3553f685b6b
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2022-08-19 17:56:00 +0300

    Add 9.9 Patientor backend, cors

--- osa9/patientor-backend/index.ts
+++ osa9/patientor-backend/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
